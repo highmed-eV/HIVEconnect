1c0a75022b38fccba57c7d083719ff2c27ce2992
chaya.rt <chaya.rt@karkinos.in>
2024-10-28 20:40:21 +0530

    new fhir bridge with camel fhir routing - added in fhir bridge engine

---
+++ fhir-bridge-engine/src/main/java/org/ehrbase/fhirbridge/security/SecurityFilterChain.java
@@ -0,0 +1,20 @@
+package org.ehrbase.fhirbridge.security;
+
+import org.springframework.context.annotation.Bean;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.DefaultSecurityFilterChain;
+
+public class SecurityFilterChain {
+    
+// @Bean
+// public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//     http
+//         .authorizeRequests()
+//             .requestMatchers("/fhir-bridge/**").permitAll()
+//             .and()
+//         .oauth2ResourceServer()
+//             .jwt();  // or remove this line if not using OAuth
+
+//     return http.build();
+// }
+}
